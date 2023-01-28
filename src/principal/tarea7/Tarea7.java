c1ca151aff7d9b7f6ab7f712069c3aa8395e27fa
FranciscoCPP <francisco.cpp@gmail.com>
2023-01-28 14:49:49 -0300

    primer commit

---
+++ src/principal/tarea7/Tarea7.java
@@ -0,0 +1,28 @@
+package principal.tarea7;
+
+
+
+public class Tarea7 {
+
+    public static void main(String[] args) {
+
+        String[] textos = {"Esto","es","muy","aburrido"};
+        String salida="";
+
+        for (String cadena :textos) {
+
+            salida+=" " + cadena;
+
+
+        }
+
+        System.out.println(salida);
+
+
+
+
+
+    }
+
+
+}
